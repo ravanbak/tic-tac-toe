d660a71d8df56fb692be1993e3c9ef0ed11c82c9
David Ravanbakhsh <ravanbak@gmail.com>
2022-01-11 10:03:55 -0800

    Add displayController module

--- game.js
+++ game.js
@@ -41,8 +41,6 @@ const game = (function(gameBoardSize) {
     
         let _squares = []; // size * size square grid
         
-        const getSize = () => size;
-    
         function reset() {
             // reset all squares to blank
             _squares = [];
@@ -54,13 +52,20 @@ const game = (function(gameBoardSize) {
                 }
             }
         }
+        reset();
     
-        const squareIsBlank = (i, j) => _squares[i][j] === markTypes.blank;
+        const getSize = () => size;
+
+        function squareIsBlank(i, j) {
+            return _squares[i][j] === markTypes.blank;
+        }
     
         function markSquare(i, j, markType) {
             _squares[i][j] = markType;
         }
     
+        const getSquareMark = (i, j) => _squares[i][j];
+
         const showSquares = () => console.table(_squares);
     
         return {
@@ -68,6 +73,7 @@ const game = (function(gameBoardSize) {
             reset,
             squareIsBlank,
             markSquare,
+            getSquareMark,
             showSquares,
         }
     })(gameBoardSize);    
@@ -75,7 +81,7 @@ const game = (function(gameBoardSize) {
     const _player1 = Player(1, markTypes.x, gameboard);
     const _player2 = Player(2, markTypes.o, gameboard);
     const getPlayer = (id) => (id === 1 ? _player1 : _player2);
-   
+
     return {
         get gameBoard() {
             return gameboard;
@@ -85,4 +91,72 @@ const game = (function(gameBoardSize) {
 
 })(GAME_BOARD_SIZE);
 
-game.gameBoard.reset();
+const displayController = (function(gameBoard) {
+    'use strict';
+
+    const _divGameboard = document.querySelector('.gameboard');
+
+    function _createGameBoard() {
+        const size = gameBoard.getSize();
+
+        for (let i = 0; i < size; i++) {
+            let divRow = document.createElement('div');
+            divRow.classList.add('gameboard__row');
+
+            for (let j = 0; j < size; j++) {
+                let divSquare = document.createElement('div');
+                divSquare.classList.add('gameboard__square');
+                divSquare.setAttribute('data-row', i);
+                divSquare.setAttribute('data-col', j);
+                divSquare.textContent = `${i}, ${j}`;
+                divSquare.style.border = '3px solid #335577';
+
+                // hide outer edge borders
+                if (i === 0) {
+                    divSquare.style.borderTop = 'none';
+                } else if (i === size - 1) {
+                    divSquare.style.borderBottom = 'none';
+                }
+                if (j === 0) {
+                    divSquare.style.borderLeft = 'none';
+                } else if (j === size - 1) {
+                    divSquare.style.borderRight = 'none';
+                }
+
+                divRow.appendChild(divSquare);
+            }
+
+            _divGameboard.appendChild(divRow);
+        }
+    }
+    _createGameBoard();
+
+    function updateGameBoard() {
+        const squares = document.querySelectorAll('.gameboard__square');
+        squares.forEach(
+            function(div) {
+                let i = div.dataset.row;
+                let j = div.dataset.col;
+                let mark = gameBoard.getSquareMark(i, j);
+
+                div.classList.remove('gameboard__square--x', 'gameboard__square--o');
+
+                switch(mark) {
+                    case markTypes.x:
+                        div.classList.add('gameboard__square--x');
+                        break;
+                    case markTypes.o:
+                        div.classList.add('gameboard__square--o');
+                        break;
+                }
+            }
+        );
+    }
+
+    return {
+        updateGameBoard,
+    }
+
+})(game.gameBoard);
+
+displayController.updateGameBoard();
