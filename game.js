1ed309a7cf11c17e9a64131f6ecfd0c70cb2e173
David Ravanbakhsh <ravanbak@gmail.com>
2022-01-10 17:11:25 -0800

    Add game module

--- game.js
+++ game.js
@@ -1,49 +1,13 @@
 "use strict";
 
+const GAME_BOARD_SIZE = 3;
+
 const markTypes = {
     // each gameboard square must contain one of the following:
     x: Symbol('x'),
     o: Symbol('o'),
     blank: null,
 }
-
-const GAME_BOARD_SIZE = 3;
-
-const gameBoard = (function(size) {
-    'use strict';
-
-    let _squares = []; // size * size square grid
-    
-    const getSize = () => size;
-
-    function reset() {
-        // reset all squares to blank
-        _squares = [];
-        for (let i = 0; i < size; i++) {
-            const row = [];
-            _squares.push(row);
-            for (let j = 0; j < size; j++) {
-                _squares[i].push(markTypes.blank);
-            }
-        }
-    }
-
-    const squareIsBlank = (i, j) => _squares[i][j] === markTypes.blank;
-
-    function markSquare(i, j, markType) {
-        _squares[i][j] = markType;
-    }
-
-    const showSquares = () => console.table(_squares);
-
-    return {
-        getSize,
-        reset,
-        squareIsBlank,
-        markSquare,
-        showSquares,
-    }
-})(GAME_BOARD_SIZE);
 
 const Player = (id, markType, gameBoard) => {
     let _score = 0;
@@ -52,21 +16,73 @@ const Player = (id, markType, gameBoard) => {
     const setMarkType = (markType) => _markType = markType;
     const reset = () => _score = 0;
     const win = () => _score++;
+    const getScore = () => _score;
 
     function takeTurn(i, j) {
         if (gameBoard.squareIsBlank(i, j)) {
             gameBoard.markSquare(i, j, markType);
         }
     }
+
     return {
         reset,
         setMarkType,
         takeTurn,
+        getScore,
         win,
     }
 }
 
-const player1 = Player(1, markTypes.x, gameBoard);
-const player2 = Player(2, markTypes.o, gameBoard);
+const game = (function(gameBoardSize) {
+    'use strict';
 
-gameBoard.reset();
+    const gameboard = (function(size) {
+        'use strict';
+    
+        let _squares = []; // size * size square grid
+        
+        const getSize = () => size;
+    
+        function reset() {
+            // reset all squares to blank
+            _squares = [];
+            for (let i = 0; i < size; i++) {
+                const row = [];
+                _squares.push(row);
+                for (let j = 0; j < size; j++) {
+                    _squares[i].push(markTypes.blank);
+                }
+            }
+        }
+    
+        const squareIsBlank = (i, j) => _squares[i][j] === markTypes.blank;
+    
+        function markSquare(i, j, markType) {
+            _squares[i][j] = markType;
+        }
+    
+        const showSquares = () => console.table(_squares);
+    
+        return {
+            getSize,
+            reset,
+            squareIsBlank,
+            markSquare,
+            showSquares,
+        }
+    })(gameBoardSize);    
+
+    const _player1 = Player(1, markTypes.x, gameboard);
+    const _player2 = Player(2, markTypes.o, gameboard);
+    const getPlayer = (id) => (id === 1 ? _player1 : _player2);
+   
+    return {
+        get gameBoard() {
+            return gameboard;
+        },
+        getPlayer,
+    }
+
+})(GAME_BOARD_SIZE);
+
+game.gameBoard.reset();
