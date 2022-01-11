6536d10078fbceadaf63c632d79147fd38c81143
David Ravanbakhsh <ravanbak@gmail.com>
2022-01-10 16:33:46 -0800

    Add player factory

--- game.js
+++ game.js
@@ -12,27 +12,61 @@ const GAME_BOARD_SIZE = 3;
 const gameBoard = (function(size) {
     'use strict';
 
-    const _size = size;
     let _squares = []; // size * size square grid
     
-    const getSize = () => _size;
+    const getSize = () => size;
 
     function reset() {
         // reset all squares to blank
         _squares = [];
-        for (let i = 0; i < _size; i++) {
+        for (let i = 0; i < size; i++) {
             const row = [];
             _squares.push(row);
-            for (let j = 0; j < _size; j++) {
+            for (let j = 0; j < size; j++) {
                 _squares[i].push(markTypes.blank);
             }
         }
     }
 
+    const squareIsBlank = (i, j) => _squares[i][j] === markTypes.blank;
+
+    function markSquare(i, j, markType) {
+        _squares[i][j] = markType;
+    }
+
+    const showSquares = () => console.table(_squares);
+
     return {
         getSize,
         reset,
+        squareIsBlank,
+        markSquare,
+        showSquares,
     }
 })(GAME_BOARD_SIZE);
+
+const Player = (id, markType, gameBoard) => {
+    let _score = 0;
+    let _markType = markType;
+
+    const setMarkType = (markType) => _markType = markType;
+    const reset = () => _score = 0;
+    const win = () => _score++;
+
+    function takeTurn(i, j) {
+        if (gameBoard.squareIsBlank(i, j)) {
+            gameBoard.markSquare(i, j, markType);
+        }
+    }
+    return {
+        reset,
+        setMarkType,
+        takeTurn,
+        win,
+    }
+}
+
+const player1 = Player(1, markTypes.x, gameBoard);
+const player2 = Player(2, markTypes.o, gameBoard);
 
 gameBoard.reset();
